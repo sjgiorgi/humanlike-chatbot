4400401266475eea17263e473c407a803a15becb
Salvatore Giorgi <sal.giorgi@gmail.com>
2025-10-30 13:43:38 -0400

    infra update

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,5 +6,8 @@ variable "db_username" {}
 variable "db_password" {}
 
 # Domain info (hosted zone must already exist in Route 53)
-variable "root_domain" { description = "Base domain in Route53, e.g., salvatoregiorgi.com" }
-variable "subdomain"   { description = "Subdomain to host ChatLab, e.g., chatlab", default = "chatlab" }
+variable "root_domain" { description = "Base domain in Route53, e.g., your-research-lab.com" }
+variable "subdomain" {
+  description = "Subdomain to host ChatLab, e.g., chatlab"
+  default     = "chatlab"
+}
