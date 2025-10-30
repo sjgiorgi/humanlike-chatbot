7aae6fd2f6e7913b8eaed0874db728f5b089dac3
Salvatore Giorgi <sal.giorgi@gmail.com>
2025-10-30 16:16:21 -0400

    more

--- infra/main.tf
+++ infra/main.tf
@@ -196,21 +196,7 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  }
 
-  zone_id = data.aws_route53_zone.root.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 60
-  records = [each.value.value]
-}
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
