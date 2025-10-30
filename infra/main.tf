b8858acf1577874be13f55494313e568e5c068ec
Salvatore Giorgi <sal.giorgi@gmail.com>
2025-10-30 15:56:25 -0400

    more

--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,12 @@
 ########################################
 
 terraform {
+  backend "s3" {
+    bucket         = "chatlab-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "chatlab-locks"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -260,4 +266,22 @@ resource "aws_cloudfront_distribution" "site" {
   price_class = "PriceClass_100"
 
   depends_on = [aws_acm_certificate_validation.cert]
+}
+
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
+  zone_id         = data.aws_route53_zone.root.zone_id
+  name            = each.value.name
+  type            = each.value.type
+  ttl             = 60
+  records         = [each.value.value]
+  allow_overwrite = true
 }
